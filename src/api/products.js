b01603519b643a76707e9e5a689f61109f9d8c1d
lmehmedagi1 <lmehmedagi1@etf.unsa.ba>
2021-03-12 18:30:04 +0100

    Sync landing page gui with api response

--- src/api/products.js
+++ src/api/products.js
@@ -12,7 +12,14 @@ class Products extends React.Component {
         axios
             .get(url)
             .then((response) => {
-                cb(null, null, response.data);
+                let products = response.data;
+                let filteredProducts = [];
+                for (let i = 0; i<products.length; i++) {
+                    let image = "https://www.firstfishonline.com/wp-content/uploads/2017/07/default-placeholder-700x700.png";
+                    if (products[i].images.length > 0) image = products[i].images[0].url;
+                    filteredProducts.push({name: products[i].name, details: products[i].details, image});
+                }
+                cb(null, null, filteredProducts);
             }).catch(error => {
                 if (error.response == null)
                     cb("Please check your internet connection!", "warning", null);
